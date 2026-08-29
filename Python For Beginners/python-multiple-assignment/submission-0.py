e203cf5da6da1131fa552d5c1368f6d0cc16bc3a
Mario Torres <mariotorreslagos@gmail.com>
2026-08-29 19:57:21 -0400

    Add: python-multiple-assignment - submission-0

---
+++ Python For Beginners/python-multiple-assignment/submission-0.py
@@ -0,0 +1,13 @@
+msg2, msg1 = "World", "Hello"
+msg4, msg5, msg3 = "Name", "Is", "My"
+# Don't change the code above this line
+
+
+
+
+# Don't change the code below this line
+print(msg1)
+print(msg2)
+print(msg3)
+print(msg4)
+print(msg5)
