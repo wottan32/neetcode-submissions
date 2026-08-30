1cdc1b1c5af9c376eb69a76c2409c6402c020d69
Mario Torres <mariotorreslagos@gmail.com>
2026-08-29 21:24:10 -0400

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,10 @@
+integer_type = 10
+float_type = 10.1
+bool_type = False
+string_type = "Hello"
+list_type =[1,2,4]
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
